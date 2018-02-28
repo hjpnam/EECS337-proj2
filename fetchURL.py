f032632416bd542562eff175c809a8e602c0b734
Hjpnam <hjpnam@gmail.com>
2018-02-27 20:22:10 -0600

    stuff

--- fetchURL.py
+++ fetchURL.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import nltk
 
 class Recipe:
 	def __init__(self, url):
 		self.page = BeautifulSoup(requests.get(url).content, 'html.parser')
-		self.ingredients = [ingredient.get_text() for ingredient in soup.find_all(itemprop="ingredients")]
-		self.directions = [direction.get_text() for direction in soup.find_all(class_="recipe-directions__list--item")]
+		self.ingredients = [ingredient.get_text() for ingredient in page.find_all(itemprop="ingredients")]
+		self.directions = [direction.get_text() for direction in page.find_all(class_="recipe-directions__list--item")]
 		self.tools = []
 		self.methods = []
+		
