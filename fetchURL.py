ac4f68f46ad587af8b7dbdcd6a6bc0928ea9ccef
SameenaK <sameenakhan2018@u.northwestern.edu>
2018-03-02 20:49:51 -0600

    get tools from recipe
    created function to find tools in the recipe, for ex "frying pan" or "blender" if it is used in a prepositional phrase

--- fetchURL.py
+++ fetchURL.py
@@ -5,8 +5,27 @@ import nltk
 class Recipe:
 	def __init__(self, url):
 		self.page = BeautifulSoup(requests.get(url).content, 'html.parser')
-		self.ingredients = [ingredient.get_text() for ingredient in page.find_all(itemprop="ingredients")]
-		self.directions = [direction.get_text() for direction in page.find_all(class_="recipe-directions__list--item")]
+		self.ingredients = [ingredient.get_text() for ingredient in self.page.find_all(itemprop="ingredients")]
+		self.directions = [direction.get_text() for direction in self.page.find_all(class_="recipe-directions__list--item")]
 		self.tools = []
 		self.methods = []
-		
+
+	def find_tools(self):
+		directions = self.directions
+		nouns = []
+		for direction in directions:
+			text = nltk.word_tokenize(direction)
+			parsed = nltk.pos_tag(text)
+			for i in range(len(parsed)):
+				if (parsed[i][0] == "in" or parsed[i][0] == "to"):
+					if (parsed[i+1][0] == "a" or parsed[i+1][0] == "the"):
+						spot = i
+						end = i+5
+						tool = []
+						while (spot<end):
+							if (parsed[spot][1] == "NN"):
+								if (parsed[spot][0] != "boil" and parsed[spot][0] != "heat"):
+									tool.append(parsed[spot][0])
+							spot += 1
+						if (len(tool)>0): nouns.append(tool)
+		return nouns
