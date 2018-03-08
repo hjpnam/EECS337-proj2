a518f9d18527ee943dbc06f3565fb0db8a902ac9
allisonsun <allisunson@gmail.com>
2018-03-07 23:41:05 -0600

    get primary and other methods

--- methods.py
+++ methods.py
@@ -1,51 +1,48 @@
+import nltk
 from nltk import pos_tag, word_tokenize, tokenize
+from Vocabulary import *
 
-PRIMARY_METHODS_LIST = ["bake", "boil", "broil", "cook", "grill", "poach", "sautee", "stir-fry"]
-OTHER_METHODS_LIST = ["chop", "crush", "cut", "dice", "drain", "grate", "grease", "melt", "mince", "mix", "pour", "preheat", "reduce", "season", "shake", "slice", "spread", "sprinkle", "squeeze", "stir"]
+def get_primary_methods(steps):
+    primary_methods_found = []
+    for step in steps:
+        for method in primary_methods:
+            if method in step.lower():
+                primary_methods_found.append(method)
+            elif method + "ing" in step.lower():
+                method = method[:-3]
+                primary_methods_found.append(method)
+            elif method + "ed" in step.lower():
+                method = method[:-2]
+                primary_methods_found.append(method)
+            elif method + "d" in step.lower():
+                method = method[:-1]
+                primary_methods_found.append(method)
+            elif method + "s" in step.lower():
+                method = method[:-1]
+                primary_methods_found.append(method)
+    return set(primary_methods_found)
 
-def get_method(step):
-    # NOT ACCURATE - adds pronoun to the beginning of each sentence
-    sentences = tokenize.sent_tokenize(direction)
-    pronoun_sentences = []
-    text = ""
-    for sentence in sentences:
-        pronoun_sentences.append("she " + sentence.lower())
+def get_other_methods(steps):
+    other_methods_found = []
+    for step in steps:
+        for method in other_methods:
+            if method in step.lower():
+                other_methods_found.append(method)
+            elif method + "ing" in step.lower():
+                method = method[:-3]
+                other_methods_found.append(method)
+            elif method + "ed" in step.lower():
+                method = method[:-2]
+                other_methods_found.append(method)
+            elif method + "d" in step.lower():
+                method = method[:-1]
+                primary_methods_found.append(method)
+            elif method + "s" in step.lower():
+                method = method[:-1]
+                primary_methods_found.append(method)
+    return set(other_methods_found)
 
-    a_text = []
-    for sentence in pronoun_sentences:
-        a_text.append(word_tokenize(sentence))
+example = [u'Bring a large pot of lightly salted water to a boil; add penne and cook, stirring occasionally, until tender yet firm to the bite, about 11 minutes. Drain.', u'Pour artichoke marinade into a large nonstick skillet over medium heat. Add chicken; cook and stir until white, about 3 minutes. Add mushrooms; cook and stir until chicken is no longer pink in the center, 4 to 6 minutes. Stir in artichokes.', u'Mix chicken broth, white wine, and cornstarch together in a bowl until cornstarch is dissolved. Pour into the skillet slowly, stirring constantly until sauce comes to a boil. Reduce heat and simmer until thickened, about 5 minutes.', u'Toss chicken mixture with penne in a large bowl. Season with salt and pepper.', u'']
 
-    for item in a_text:
-        print pos_tag(item)
-    return pronoun_sentences
-
-def get_primary_methods(step):
-    primary_methods = []
-    for method in PRIMARY_METHODS_LIST:
-        if method in step:
-            primary_methods.append(method)
-        elif method + "ing" in step:
-            method = method[:-3]
-            primary_methods.append(method)
-        elif method + "ed" in step:
-            method = method[:-2]
-            primary_methods.append(method)
-    return set(primary_methods)
-
-def get_other_methods(step):
-    other_methods = []
-    for method in OTHER_METHODS_LIST:
-        if method in step:
-            other_methods.append(method)
-        elif method + "ing" in step:
-            method = method[:-3]
-            other_methods.append(method)
-        elif method + "ed" in step:
-            method = method[:-2]
-            other_methods.append(method)
-    return set(other_methods)
-
-sentence = "In a skillet, cook sausage, onion and garlic until pork is no longer pink and onion is tender; drain. Add marinara sauce and oregano; simmer for 5 minutes. In a bowl, combine ricotta cheese, egg, 1/4 cup Parmesan and spinach. In a greased 13-in. x 9-in. x 2-in. baking dish, spread 1 cup meat sauce. Arrange 3 noodles over sauce. Spread one-fourth of the ricotta cheese mixture over the noodles, top with 1 cup of meat sauce. Sprinkle with 1/2 cup mozzarella cheese. Repeat process 3 times. Top with remaining Parmesan cheese. Bake, uncovered, at 350 degrees F for 40 to 45 minutes. Let stand for 10 minutes before cutting. Serve."
-
-print get_primary_methods(sentence.lower())
-print get_other_methods(sentence.lower())
+print "primary methods", get_primary_methods(example)
+print "other methods", get_other_methods(example)
